d3110a9d630a60b3d710661c9497a2d6a254db64
phernandez <paul@basicmachines.co>
2026-09-03 13:15:27 -0500

    feat(api): add batched accepted note reads
    Signed-off-by: phernandez <paul@basicmachines.co>

--- src/basic_memory/api/v2/routers/__init__.py
+++ src/basic_memory/api/v2/routers/__init__.py
@@ -1,5 +1,8 @@
 """V2 API routers."""
 
+from basic_memory.api.v2.routers.accepted_content_router import (
+    router as accepted_content_router,
+)
 from basic_memory.api.v2.routers.knowledge_router import router as knowledge_router
 from basic_memory.api.v2.routers.project_router import router as project_router
 from basic_memory.api.v2.routers.memory_router import router as memory_router
@@ -12,6 +15,7 @@ from basic_memory.api.v2.routers.schema_router import router as schema_router
 from basic_memory.api.v2.routers.inspect_router import router as inspect_router
 
 __all__ = [
+    "accepted_content_router",
     "knowledge_router",
     "project_router",
     "memory_router",
